bc9e30851ac0860f26a54eb325aa85cc483e329b
Maxim <caliber1997@gmail.com>
2016-12-19 13:35:57 +0100

    Updated diagram generator to add _ instead of commas

--- fmse_tool/cli/diagram_generator.py
+++ fmse_tool/cli/diagram_generator.py
@@ -4,9 +4,9 @@ from graphviz import Digraph
 def generate_diagram(lts, filename):
     dot = Digraph()
     for state in set(lts.get_states()):
-        dot.node(', '.join(state), style='filled', fillcolor='grey' if state == lts.initial_state else 'white')
+        dot.node('_'.join(state), style='filled', fillcolor='grey' if state == lts.initial_state else 'white')
     for from_state, token, to_state in lts.transitions:
-        dot.edge(', '.join(from_state), ', '.join(to_state), label=token)
+        dot.edge('_'.join(from_state), '_'.join(to_state), label=token)
     dot.render(filename, directory='result')
 
 
@@ -14,7 +14,7 @@ def generate_extended_diagram(lts, valid_states, filename=None):
     dot = Digraph()
     for state in set(lts.get_states()):
         dot.node(
-            ', '.join(state),
+            '_'.join(state),
             style='filled',
             fillcolor='darkgreen' if state == lts.initial_state and state in valid_states
                 else 'green' if state in valid_states
@@ -24,7 +24,7 @@ def generate_extended_diagram(lts, valid_states, filename=None):
             forcelabels='True'
         )
     for from_state, token, to_state in lts.transitions:
-        dot.edge(', '.join(from_state), ', '.join(to_state), label=token)
+        dot.edge('_'.join(from_state), '_'.join(to_state), label=token)
     if filename is not None:
         dot.render(filename, directory='result')
     else:
